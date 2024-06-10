8cf772446f7ba175e545c6486a88315f769f6608
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-10 17:15:53 +0200

    feat: cards showed in "movies.blade.php" and minor fix to style

--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,5 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-    
+    <div class="container">
+        <div class="row d-flex">
+            @foreach ($movies as $movie)
+                <div class="col g-4">
+                    <div class="card" style="width: 18rem;">
+                        <img class="card-img-top" src="{{ $movie->image }}" alt="Card image cap">
+                        <div class="card-body">
+                        <h3 class="card-title">{{ $movie->title }}</h3>
+                        <h4 class="card-text">Titolo originale: {{ $movie->original_title }}</h4>
+                        <h5 class="card-text">Voto: {{ $movie->vote }}</h5>
+                        </div>
+                    </div>
+                </div>
+                @endforeach
+        </div>
+    </div>
 @endsection
