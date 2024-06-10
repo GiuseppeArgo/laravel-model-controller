19334c98fed66384d9bcbd7de54d4c80a9aee04d
GiuseppeArgo <giuseppe.argo18@gmail.com>
2024-06-10 17:35:03 +0200

    feat: project fixed layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,10 @@
 </head>
 
 <body>
+  <header>
+    @yield('header')
+  </header>
+
   <main>
     @yield('content')
   </main>
--- resources/views/movies.blade.php
+++ resources/views/movies.blade.php
@@ -1,20 +1,20 @@
 @extends('layouts.app')
 
+@section('header')
+    <h2>Boolflix</h2>
+@endsection
+
 @section('content')
-    <div class="container">
-        <div class="row d-flex">
-            @foreach ($movies as $movie)
-                <div class="col g-4">
-                    <div class="card" style="width: 18rem;">
-                        <img class="card-img-top" src="{{ $movie->image }}" alt="Card image cap">
-                        <div class="card-body">
-                        <h3 class="card-title">{{ $movie->title }}</h3>
-                        <h4 class="card-text">Titolo originale: {{ $movie->original_title }}</h4>
-                        <h5 class="card-text">Voto: {{ $movie->vote }}</h5>
-                        </div>
-                    </div>
+    <div class="ms_container">
+         @foreach ($movies as $movie)
+            <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="{{ $movie->image }}" alt="Card image cap">
+                <div class="card-body">
+                    <h3 class="card-title">{{ $movie->title }}</h3>
+                    <h4 class="card-text">Titolo originale: {{ $movie->original_title }}</h4>
+                    <h5 class="card-text">Voto: {{ $movie->vote }}</h5>
                 </div>
-                @endforeach
-        </div>
+            </div>
+        @endforeach
     </div>
 @endsection
